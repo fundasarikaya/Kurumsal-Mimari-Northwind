a1522d24509f4faa954f90eefd01ad4188a2eb35
fundasarikaya <fundamelihcan@gmail.com>
2021-01-09 20:43:00 +0300

    first commit

---
+++ Northwind.Dal/Abstract/IProductDal.cs
@@ -0,0 +1,22 @@
+﻿using Northwind.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Dal.Abstract
+{
+   public interface IProductDal
+    {
+        //veri erişim katmanı
+        //orn ürün listelenir eklenir silinr vb
+
+        List<Product> GetAll();
+        //Product referencess ekledik
+        IProductDal Get(int productId);
+        void Add(Product product);
+        void Delete(int productId);
+        void Update(Product product);
+    }
+}
